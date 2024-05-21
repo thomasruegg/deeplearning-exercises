d9b2542239eb823f377c80310064cacedf7da229
thomasruegg <hello@thomasruegg.com>
2024-05-21 11:12:22 +0200

    week14 done

--- week14/practice/model.py
+++ week14/practice/model.py
@@ -13,7 +13,25 @@ class MyModel(Model):
 
     def __init__(self, **kwargs):
         super(MyModel, self).__init__(**kwargs)
-        # TODO: Instantiate model / layers
+
+        IMAGE_SHAPE = (300, 300, 3)
+
+        self.preprocess_layer = preprocess_input
+
+        self.backbone = tf.keras.applications.ResNet50(
+            include_top=False, weights="imagenet", input_shape=IMAGE_SHAPE
+        )
+        self.backbone.trainable = False
+
+        self.avg_pool_1 = GlobalAveragePooling2D()
+
+        self.drop_1 = Dropout(0.5)
+        self.drop_2 = Dropout(0.5)
+        self.drop_3 = Dropout(0.5)
+
+        self.dense_1 = Dense(1024, activation="relu")
+        self.dense_2 = Dense(64, activation="relu")
+        self.dense_3 = Dense(3)
 
     def call(self, x, training=False):
         """
@@ -33,8 +51,19 @@ class MyModel(Model):
         Tensor (None, 1)
             Logits of classifier (pre-softmax activations)
         """
-        out = x
-        # TODO: Define forward pass through network
+        x = self.preprocess_layer(x)
+        x = self.backbone(x)
+        x = self.avg_pool_1(x)
+
+        x = self.drop_1(x, training=training)
+        x = self.dense_1(x)
+
+        x = self.drop_2(x, training=training)
+        x = self.dense_2(x)
+
+        x = self.drop_3(x, training=training)
+        out = self.dense_3(x)
+
         return out
 
 
